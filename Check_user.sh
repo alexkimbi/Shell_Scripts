9e6ea785f378ef7a912379f6ce4908e02d87ad2f
Alex kimbi <alexkimbi@hotmail.com>
2022-09-10 15:20:46 -0400

    First commit to this check file

---
+++ Check_user.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+echo "What is yur first name?"
+read Fname
+if [ $Fname != John ] 
+then 
+	echo " Your entered the wrong First name"
+else 
+	echo " You seems to be the same $Fname that i know"
+fi
+	echo "Thanks and bye"
+
+
